85d8f4b79391bbc93567b043623c3285a7108bb4
Crislayne <crissoaresr@hotmail.com>
2023-11-28 12:10:27 -0300

    image react

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import Cardapio_card from './components/cardapio-card/Cardapio-card'
 import Cover from './components/cover/Cover.jsx'
 import { createGlobalStyle } from 'styled-components'
 
+import LaCuma from './assets/la-cuma.png'
+import LaMenta from './assets/la-menta.png'
+import LaBana from './assets/la-bana.png'
+import Margarita from './assets/margarita.png'
+
 function App() {
 
   const EstiloGlobal = createGlobalStyle `
@@ -27,22 +32,22 @@ function App() {
         <h2>Mais vendidos</h2>
         <section className='container__cardapio'>
           <Cardapio_card
-            imgUrl={"/src/assets/la-cuma.png"}
+            imgUrl={LaCuma}
             title={"La Cuma"}
             price={"23"}
           ></Cardapio_card>
           <Cardapio_card
-            imgUrl={"/src/assets/la-menta.png"}
+            imgUrl={LaMenta}
             title={"La Menta"}
             price={"24"}
           ></Cardapio_card>
           <Cardapio_card
-            imgUrl={"/src/assets/la-bana.png"}
+            imgUrl={LaBana}
             title={"La Bana"}
             price={"25"}
           ></Cardapio_card>
           <Cardapio_card
-            imgUrl={"/src/assets/margarita.png"}
+            imgUrl={Margarita}
             title={"La Toca"}
             price={"26"}
           ></Cardapio_card>
